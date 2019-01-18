989119afbfed3d998997fbcac31ba250880b7119
specialistik <specialistik@localhost.localdomain>
2019-01-18 13:24:20 -0500

    cleaning up and package

--- components/ta-grid-item.js
+++ components/ta-grid-item.js
@@ -4,15 +4,14 @@ import { ListItem } from 'react-native-elements';
 export default class TAGridItem extends Component {
     constructor(props) {
         super(props);
-        console.log('grid item props are ', props)
-        //<ListItem title={this.props.title} badge={{ value: this.props.timing }}/>
+        console.log('grid item props are ', props);
     }
 
     render() {
         return (
             <ListItem 
-                title={this.props.row_title} 
-                badge={this.props.timing} 
+                title={ this.props.row_title }
+                badge={ this.props.timing } 
             />
         )
     }
--- components/ta-grid.js
+++ components/ta-grid.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { ListItem } from 'react-native-elements';
-import TAGridItem from './ta-grid-item';
 
 export default class TAGrid extends Component {
     constructor(props) {
         super(props);
         console.log('the grid constructor props R ', props);
-
     }
 
     render() {
         return (
             <View>
-                
                 {(this.props.items && this.props.items.length > 0) ?
                     this.props.items.forEach(function(val,index) { 
                         <ListItem 
                             title={ val } 
                             badge={ index }
                         />
-                    }):<ListItem title="Остановка" badge="22.40" />
+                    }) :  <ListItem title="Остановка" badge="22.40" />
                 }
             </View>
         )
