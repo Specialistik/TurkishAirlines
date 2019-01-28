03c4e283fbb2ec7c94e12cc13d6fa503a9134128
Specialistik <test04@list.ru>
2019-01-28 15:51:38 +0300

    just a thing

--- components/main.js
+++ components/main.js
@@ -8,7 +8,8 @@ export default class Main extends Component {
     
     this.statuses = {
       'plane_stop': {
-        title: 'Остановка'
+        title: 'Остановка',
+        depends: []
       },
       'plane_open': {
         title: 'Открытие',
@@ -64,10 +65,10 @@ export default class Main extends Component {
       }
     }
 
+    this.used_statuses = [];
     this.statuses_flat = ['plane_stop', 'plane_open', 'debarkment', 'cleaning', 'discharge', 'refueling', 'meals', 'ma7', 'water', 'readyness', 'embarkation', 'upload', 'plane_close', 'mooring_tractor'];
 
     this.state = {
-      status_list: [],
       status_grid: [],
       button_list: [
         'plane_stop'
@@ -75,62 +76,85 @@ export default class Main extends Component {
     }
 
     this.updateStatus = this.updateStatus.bind(this);
-    this.buttonShouldRender = this.buttonShouldRender.bind(this);
+    this.dependenciesWerePicked = this.dependenciesWerePicked.bind(this);
   }
 
-  buttonShouldRender(selectedStatus) {
-    if (this.state.status_list && this.state.status_list.length > 0) {
-      this.statuses_flat.map((index, val) => {
-        if (val == selectedStatus) {
-          return true;
-        }
-        return false;
-      });
+  addNullIfThisIsWhatHaveBeenWanted(time) {
+    if (time < 10) {
+      return "0" + str(time);
     } else {
-      if (selectedStatus == 'plane_stop')
-        return true;
+      return time;
     }
+  }
+
+  // Зависимости статуса уже были кликнуты, пока только для одной зависимости
+  dependenciesWerePicked(status) {
+    if (this.statuses[status]['depends'].length == 0) {
+      return true;
+    }
+
+    if (status == 'plane_open') {
+      console.log('-=-----', this.statuses[status]['depends']);
+    }
+
+    this.statuses[status]['depends'].map((val, index) => {
+      console.log('dependency lit ', val);
+      if (this.used_statuses.indexOf(val) == status) {
+        return true;
+      }
+    });
     return false;
   }
 
   updateStatus(selectedStatus) {
     let button_list = [];
-    this.statuses_flat.map((index, val) => {
+    this.used_statuses = [...this.used_statuses, selectedStatus];
+
+    this.statuses_flat.map((val, index) => {
+      //console.log('status ', val, ' has happened ', this.statusHappened(val));
+      //console.log('dependencies were picked =', this.dependenciesWerePicked(val));
+
       // Если выбранного статуса нет в списке отмеченных И если выбранный статус был в зависимости у того, по которому проходим 
-      if ((this.buttonShouldRender(val) <= 0) && (this.statuses[val].depends.indexOf(selectedStatus) >= 0)) {
+      if ((!(this.used_statuses.indexOf(val) >= 0)) && this.dependenciesWerePicked(val)) {
         button_list.push(val);
       } 
     });
+  
     /*
-    if (selectedStatus == 'plane_stop') {
-      button_list = ['plane_open'];
-    }
-     
-    if (selectedStatus == 'plane_open') {
-      button_list = [
-        'debarkment',
-        'discharge',
-        'water',
-        'ma7'
-      ]
-    }
-
-    ['debarkment', 'discharge', 'water', 'ma7'].map((val, index) => {
-      if ((val != selectedStatus) && !(this.state.status_list.indexOf(val) > 0)) {
-        button_list.push(val);
+    switch (selectedStatus) {
+      case 'plane_stop': {/ N 
+        button_list = ['plane_open'];
+        break;
       }
-    })
-    */
+      
+      case 'plane_open': {
+        button_list = [
+          'debarkment',
+          'discharge',
+          'water',
+          'ma7'
+        ];
+        break;
+      }
+      case 'debarkment': {
+        
+      }
+      ['debarkment', 'discharge', 'water', 'ma7'].map((val, index) => {
+        if ((val != selectedStatus) && !(this.state.status_list.indexOf(val) > 0)) {
+          button_list.push(val);
+        }
+      })
+    } 
+   */
 
     let currentdate = new Date(); 
     let normalized_data = { 
-      timing: currentdate.getHours() + '.' + currentdate.getMinutes(),
+      timing: currentdate.getHours() + '.' + this.addNullIfThisIsWhatHaveBeenWanted(currentdate.getMinutes()),
       status: this.statuses[selectedStatus].title
     }
 
     this.setState(prevState => ({
       status_grid: [...prevState.status_grid, normalized_data],
-      status_list: [...prevState.status_list, selectedStatus],
       button_list: button_list,
     }));
   }
