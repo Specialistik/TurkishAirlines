e9e2fc8341845eff259f261581c84f85ff811602
Specialistik <test04@list.ru>
2019-01-29 04:40:29 +0300

    some webpack and dev server issues

--- components/main.js
+++ components/main.js
@@ -9,59 +9,59 @@ export default class Main extends Component {
     this.statuses = {
       'plane_stop': {
         title: 'Остановка',
-        depends: []
+        'depends': []
       },
       'plane_open': {
         title: 'Открытие',
-        depends: ['plane_stop']
+        'depends': ['plane_stop'] 
       },
       'debarkment': {
         title: 'Высадка',
-        depends: ['plane_open']
+        'depends': ['plane_open']
       },
       'cleaning': {
         title: 'Уборка',
-        depends: ['debarkment']
+        'depends': ['debarkment']
       },
       'discharge': {
         title: 'Разгрузка',
-        depends: ['plane_open']
+        'depends': ['plane_open']
       },
       'refueling': {
         title: 'Заправка',
-        depends: ['debarkment']
+        'depends': ['debarkment']
       },
       'meals': {
         title: 'Борт. питание',
-        depends: ['debarkment']
+        'depends': ['debarkment']
       },
       'ma7': {
         title: 'МА-7',
-        depends: ['plane_open']
+        'depends': ['plane_open']
       },
       'water': {
         title: 'Вода',
-        depends: ['plane_open']
+        'depends': ['plane_open']
       },
       'readyness': {
         title: 'Готовность',
-        depends: ['meals', 'cleaning', 'refueling']
+        'depends': ['meals', 'cleaning', 'refueling']
       },
       'embarkation': {
         title: 'Посадка',
-        depends: ['readyness']
+        'depends': ['readyness']
       },
       'upload': {
         title: 'Загрузка',
-        depends: ['discharge']
+        'depends': ['discharge']
       },
       'plane_close': {
         title: 'Закрытие',
-        depends: ['upload', 'discharge']
+        'depends': ['upload', 'discharge']
       },
       'mooring_tractor': {
         title: 'Швартовка тягача',
-        depends: ['plane_close']
+        'depends': ['plane_close']
       }
     }
 
@@ -70,9 +70,7 @@ export default class Main extends Component {
 
     this.state = {
       status_grid: [],
-      button_list: [
-        'plane_stop'
-      ]
+      button_list: ['plane_stop']
     }
 
     this.updateStatus = this.updateStatus.bind(this);
@@ -81,7 +79,7 @@ export default class Main extends Component {
 
   addNullIfThisIsWhatHaveBeenWanted(time) {
     if (time < 10) {
-      return "0" + str(time);
+      return "0" + time;
     } else {
       return time;
     }
@@ -93,13 +91,14 @@ export default class Main extends Component {
       return true;
     }
 
-    if (status == 'plane_open') {
-      console.log('-=-----', this.statuses[status]['depends']);
-    }
-
     this.statuses[status]['depends'].map((val, index) => {
-      console.log('dependency lit ', val);
-      if (this.used_statuses.indexOf(val) == status) {
+      if (val == 'plane_stop') {
+        console.log('status is ', status);
+        console.log('used statuses are ', this.used_statuses);
+        console.log('used statuses include plane_stop ', this.used_statuses.includes(val));
+      }
+
+      if (this.used_statuses.includes(val)) {
         return true;
       }
     });
@@ -111,42 +110,18 @@ export default class Main extends Component {
     this.used_statuses = [...this.used_statuses, selectedStatus];
 
     this.statuses_flat.map((val, index) => {
-      //console.log('status ', val, ' has happened ', this.statusHappened(val));
-      //console.log('dependencies were picked =', this.dependenciesWerePicked(val));
-
-      // Если выбранного статуса нет в списке отмеченных И если выбранный статус был в зависимости у того, по которому проходим 
-      if ((!(this.used_statuses.indexOf(val) >= 0)) && this.dependenciesWerePicked(val)) {
+      if (val == 'plane_open') {
+        console.log('dependencies were picked =', this.dependenciesWerePicked(val));
+        console.log('status not used ', (!(this.used_statuses.includes(val))) )
+      }
+      // Если выбранного статуса нет в списке уже использованных И если выбранный статус был в зависимости у того, по которому проходим 
+      if ((!(this.used_statuses.includes(val))) && this.dependenciesWerePicked(val)) {
+        console.log('button should appear ', val);
         button_list.push(val);
       } 
     });
-  
-    /*
-    switch (selectedStatus) {
-      case 'plane_stop': {/ N 
-        button_list = ['plane_open'];
-        break;
-      }
-      
-      case 'plane_open': {
-        button_list = [
-          'debarkment',
-          'discharge',
-          'water',
-          'ma7'
-        ];
-        break;
-      }
-      case 'debarkment': {
-        
-      }
-      ['debarkment', 'discharge', 'water', 'ma7'].map((val, index) => {
-        if ((val != selectedStatus) && !(this.state.status_list.indexOf(val) > 0)) {
-          button_list.push(val);
-        }
-      })
-    } 
-   */
 
+    
     let currentdate = new Date(); 
     let normalized_data = { 
       timing: currentdate.getHours() + '.' + this.addNullIfThisIsWhatHaveBeenWanted(currentdate.getMinutes()),
--- index.js
+++ index.js
@@ -3,3 +3,10 @@ import { name as appName } from './app.json';
 import Main from './components/main';
 
 AppRegistry.registerComponent(appName, () => Main);
+
+if (window.document) {
+    AppRegistry.runApplication(appName, {
+        initialProps: {},
+        rootTag: document.getElementById("react-root")
+    });
+}
